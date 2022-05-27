1758528354fc1bf78a8a18d29cafee37d7d1d569
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2022-05-27 21:34:46 +0700

    Add Route page 404

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
+import Page404 from "./Pages/Page404";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="*" element={<Page404 />} />
       </Routes>
     </BrowserRouter>
   );
