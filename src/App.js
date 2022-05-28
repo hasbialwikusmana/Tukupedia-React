f1a5c90ef036cc95ba248c1331f11f24feeeb3f8
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2022-05-28 19:13:47 +0700

    rename

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Pages/Home/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./Pages/Home";
+import Login from "./Pages/Auth/Login";
+import Register from "./Pages/Auth/Register";
+import ResetPassword from "./Pages/Auth/ResetPassword";
 import Page404 from "./Pages/Page404";
 
 function App() {
@@ -8,6 +12,9 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/auth/login" element={<Login />} />
+        <Route path="/auth/register" element={<Register />} />
+        <Route path="/auth/reset-password" element={<ResetPassword />} />
         <Route path="*" element={<Page404 />} />
       </Routes>
     </BrowserRouter>
--- src/Pages/Home/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Home = () => {
-  return (
-    <div><h1>Welcome to page Home</h1></div>
-  )
-}
-
-export default Home
