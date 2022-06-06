648e76915af80bdf649c7f172803ee8d788b9ccb
hasbialwikusmana <hasbialwi70@gmail.com>
2022-06-06 22:47:36 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,8 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Home from "./Pages/Home";
-// import Login from "./Pages/Auth/Login";
-// import Register from "./Pages/Auth/Register";
-// import ResetPassword from "./Pages/Auth/ResetPassword";
-// import MyBag from "./Pages/MyBag";
-// import Checkout from "./Pages/Checkout";
-// import Custommer from "./Pages/Custommer";
-// import Seller from "./Pages/Seller";
-import Page404 from "./Pages/Page404";
+import Router from "./Configs/Router";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        {/* <Route path="/" element={<Home />} />
-        <Route path="/auth/login" element={<Login />} />
-        <Route path="/auth/register" element={<Register />} />
-        <Route path="/auth/resetPassword" element={<ResetPassword />} />
-        <Route path="/mybag" element={<MyBag />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/custommer" element={<Custommer />} />
-        <Route path="/seller" element={<Seller />} /> */}
-        <Route path="*" element={<Page404 />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Router />;
 }
 
 export default App;
